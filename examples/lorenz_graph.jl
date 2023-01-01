d35d06bbf06e57c92518927b1eeec40e894d5277
Andre Souza <andrenogueirasouza@gmail.com>
2023-01-01 15:23:08 -0600

    preliminary graph done

--- examples/lorenz_graph.jl
+++ examples/lorenz_graph.jl
@@ -18,8 +18,8 @@ timeseries = Vector{Float64}[]
 markov_chain = Int64[]
 initial_condition = [14.0, 20.0, 27.0]
 push!(timeseries, initial_condition)
-dt = 0.01
-iterations = 10^6
+dt = 0.005
+iterations = 10^7
 
 markov_index = argmin([norm(initial_condition - markov_state) for markov_state in markov_states])
 push!(markov_chain, markov_index)
@@ -35,66 +35,71 @@ end
 Q = generator(markov_chain, 3; dt=dt)
 
 ##
+fig = Figure(resolution=(1500, 1000))
+ax_Q = Axis(fig[1, 1]; title="Generator", titlesize=30)
 
-cmap = :balance # :Blues_9
-cmapa = to_colormap(cmap);
-# cmap = vcat(cmapa[1:15], fill(RGBAf(0, 0, 0, 0), 10), cmapa[25:end])
-
-fig = Figure(resolution=(1506, 1076))
-ax = Axis(fig[1, 1]; title="Transition Probability", titlesize=30)
-ax_Q = Axis(fig[1, 2]; title="Transition Rate", titlesize=30)
-
-dt_slider = Slider(fig[2, 1:2], range=0:0.01:2, startvalue=0)
-dt = dt_slider.value
-
-# Q = uniform_phase(4)
-T = @lift exp(Q * $dt)
-
-g = DiGraph(exp(Q))
+# Generator
 g_Q = DiGraph(Q)
-
-elabels = [string(Q[i])[1:5] for i in 1:ne(g)]
-
-elabels_T = @lift ([string($T[i]) for i in 1:ne(g)])
-
-edge_color = @lift [RGBAf(cmapa[4].r, cmapa[4].g, cmapa[4].b, $T[i] / 0.44) for i in 1:ne(g)]
-edge_width = [4.0 for i in 1:ne(g)]
-arrow_size = [30.0 for i in 1:ne(g)]
-node_labels = repr.(1:nv(g))
-
-# edge_color_Q = [RGBAf(cmapa[4].r, cmapa[4].g, cmapa[4].b, 1.0) for i in 1:ne(g_Q)]
-# for opacity do (:red, 0.5)
 edge_color_Q = [:red, :red, :red, :blue, :blue, :blue, :orange, :orange, :orange]
+elabels = [string(Q[i])[1:5] for i in 1:ne(g_Q)]
 elabels_color = edge_color_Q
 node_color = [:red, :blue, :orange]
 edge_attr = (; linestyle=[:dot, :dash, :dash, :dash, :dot, :dash, :dash, :dash, :dot])
-elabels_fontsize = 40
-edge_width_Q = [4.0 for i in 1:ne(g_Q)]
-arrow_size_Q = [30.0 for i in 1:ne(g_Q)]
-node_labels_Q = repr.(1:nv(g_Q))
+elabels_fontsize = 30
+nlabels_fontsize = 30
 node_size = 30.0
 
-# obs_string = @lift("Transition Probability at time t = " * string($dt) )
-p = graphplot!(ax, g, elabels=elabels_T, edge_color=edge_color, edge_width=edge_width,
-    arrow_size=arrow_size, node_size=node_size,
-    nlabels=node_labels, nlabels_textsize=50.0)
+edge_width_Q = [2.0 for i in 1:ne(g_Q)]
+arrow_size_Q = [20.0 for i in 1:ne(g_Q)]
+node_labels_Q = repr.(1:nv(g_Q))
+node_labels_Q = ["Right Lobe", "Origin", "Left Lobe"]
 
-offsets = 0.15 * (p[:node_pos][] .- p[:node_pos][][1])
-offsets[1] = Point2f(0, 0.3)
-p.nlabels_offset[] = offsets
-autolimits!(ax)
-hidedecorations!(ax)
 
 p_Q = graphplot!(ax_Q, g_Q, elabels=elabels, elabels_color=elabels_color,
     elabels_fontsize=elabels_fontsize, edge_color=edge_color_Q,
     edge_width=edge_width_Q, node_color=node_color,
     arrow_size=arrow_size_Q, node_size=node_size,
-    nlabels=node_labels_Q, nlabels_textsize=50.0)
+    nlabels=node_labels_Q, nlabels_fontsize=nlabels_fontsize)
 
-offsets = 0.15 * (p_Q[:node_pos][] .- p_Q[:node_pos][][1])
-offsets[1] = Point2f(0, 0.3)
+offsets = 0.0 * (p_Q[:node_pos][] .- p_Q[:node_pos][][1])
+offsets[2] = Point2f(-0.5, -0.5)
 p_Q.nlabels_offset[] = offsets
 autolimits!(ax_Q)
-
 hidedecorations!(ax_Q)
+
+# t = 0.1 Transfer operator
+dt = 0.1
+dt_strings = ["0.1", "0.5", "2"]
+dt_vals = parse.(Float64, dt_strings)
+for (ii, dt) in enumerate(dt_vals)
+    j = (ii) % 2 + 1
+    i = (ii) ÷ 2 + 1
+    ax_T1 = Axis(fig[i, j]; title="Transfer Operator for t = " * dt_strings[ii], titlesize=30)
+    T1 = exp(Q * dt)
+    g_T1 = DiGraph(T1)
+    elabels = [string(T1[i])[1:5] for i in 1:ne(g_T1)]
+    edge_color_T1 = [(edge_color, T1[i] / 0.5) for (i, edge_color) in enumerate(edge_color_Q)]
+    elabels_color = edge_color_Q
+    node_color = [:red, :blue, :orange]
+    edge_attr = (; linestyle=[:dot, :dash, :dash, :dash, :dot, :dash, :dash, :dash, :dot])
+    edge_width_Q = [2.0 for i in 1:ne(g_T1)]
+    arrow_size_Q = [20.0 for i in 1:ne(g_T1)]
+    node_labels_Q = repr.(1:nv(g_Q))
+    node_labels_Q = ["Right Lobe", "Origin", "Left Lobe"]
+
+    p_Q = graphplot!(ax_T1, g_T1, elabels=elabels, elabels_color=elabels_color,
+        elabels_fontsize=elabels_fontsize, edge_color=edge_color_T1,
+        edge_width=edge_width_Q, node_color=node_color,
+        arrow_size=arrow_size_Q, node_size=node_size,
+        nlabels=node_labels_Q, nlabels_fontsize=nlabels_fontsize)
+
+    offsets = 0.0 * (p_Q[:node_pos][] .- p_Q[:node_pos][][1])
+    offsets[2] = Point2f(-0.5, -0.5)
+    p_Q.nlabels_offset[] = offsets
+    autolimits!(ax_T1)
+    hidedecorations!(ax_T1)
+end
+
 display(fig)
+##
+save("lorenz_graph.png", fig)
